576a1e0eb00b60cb7d385c8ed12e51eb650fbcea
francisco.kennedi <francisco.kennedi@esig.com.br>
2021-01-03 23:41:52 -0300

    corrigindo layout landing e adicionando links para os shops

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,47 +1,98 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './styles.css'
 import { Link } from 'react-router-dom'
 import landingImg from '../../assets/images/catalogo.jpg';
 import StoreContext from '../../components/Store/Context';
+import api from '../../services/api';
+import whatsappIcon from '../../assets/images/whatsappIcon.png'
 
+interface ShopProps {
+    shop: {
+        name: string,
+        bio: string,
+        whatsapp: string,
+        tag: string,
+    }
+}
 
 function Landing() {
     const { token } = useContext(StoreContext);
+    const [shops, setShops] = useState([]);
+
+    useEffect(() => {
+        function findAllShops() {
+            api.get('/shops')
+            .then((response) => {
+                setShops(response.data);
+            })
+        }
+
+        findAllShops()
+    }, []);
     return (
         <div id="page-landing">
             <div className="logo-container">
                 <a href="/" className="logo">Catalogue.me</a>
                 <div className="nav">
-                    <Link to={token ? "/dashboard/admin/inicio" : "/user/login"}>{token ? "Dashboard" : "Login"}</Link>
+                    <Link to={token ? "/dashboard/admin/inicio" : "/user/login"}>{token ? "Loja" : "Login"}</Link>
                     {token ? "" : <Link to="/user/form">Cadastro</Link>}
                 </div>
             </div>
             <div id="page-landing-content">
                 <div className="landing">
                     <h2 className="subtitle">Crie seu catálogo virtual online e 100% grátis em menos de 5 minutos.</h2>
-                    <br/>
+                    
+                    <p id="text3">
+                        Modernize sua loja virtual com o nosso catálogo, não precisa instalar nada! Faça login, cadastre seus produtos/serviços e compartilhe com seus clientes em tempo real.
+                    </p>
+                    
+                    
                     <div className="buttons-area">
-                        <p className="text" id="text3">
-                            Modernize sua loja virtual com o nosso catálogo, não precisa instalar nada! Faça login, cadastre seus produtos/serviços e compartilhe com seus clientes em tempo real.
-                        </p>
                         <div className="buttons-container">
-                            <Link to="/user-form" className="products">
+                            <Link to="/user/form" className="products">
                                 Cadastre o seu catálogo virtual agora clicando aqui.
                             </Link>
                         </div>
                     </div>
                 
-                    <p className="text" id="text1">
-                        Com o Catalogue.me você cadastra seus itens em um catálogo virtual e envia para seus clientes, com a facilidade de um clique!
-                    </p>
+                    
                     <div className="landing-image">
-                        <img src={landingImg} className="hero-image" alt="landing"/>
+                    <p className="text" id="text1">
+                        Veja abaixo algumas lojas que já aderiram ao Catalogue.me
+                    </p>
+                    <h2>Lojas associadas</h2>
+                        <div className="landing-shops">
+                            
+                            {
+                            shops.map((shops: ShopProps["shop"]) => {
+                                return(
+                                    <div className="shop-card">
+                                        <div className="info">
+                                            <h3>{shops.name}</h3>
+                                            <p>{shops.bio}</p>
+                                        </div>
+                                        <div className="buttons">
+                                            <a rel="noopener noreferrer" href={ process.env.REACT_APP_URL+'/'+shops.tag }>
+                                            
+                                                Visitar a loja
+                                            </a>
+                                            <a target="_blank" rel="noopener noreferrer" href={'https://wa.me/+55' + shops.whatsapp }>
+                                            <img src={whatsappIcon} alt="whatsapp"/>
+                                                Entrar em contato
+                                            </a>
+                                        </div>
+                                    </div>
+                                );
+                            })
+                            }
+                        </div>
                     </div>
                     <p className="text" id="text2">
-                        Modernize o seu comércio agora mesmo e comece a usar o Catalogue.me! É a praticidade que você estava procurando. 
+                        Comece a usar o Catalogue.me agora mesmo clicando no botão abaixo! É a praticidade que você estava procurando. 
                     </p>
                     
                 </div> 
+                
             </div>
             
             <div className="footer-container">
