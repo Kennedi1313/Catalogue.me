9d101761723fd3318190c2b7bad648833c316218
francisco.kennedi <francisco.kennedi@esig.com.br>
2021-02-08 18:39:11 -0300

    add capture

--- src/pages/Dashboard/AddItem-Dashboard/index.tsx
+++ src/pages/Dashboard/AddItem-Dashboard/index.tsx
@@ -116,8 +116,6 @@ const AddItem: React.FC<Props> = ({categories}) => {
                         name="avatar" 
                         label="Imagem de Capa (mais imagens poderão ser adicionadas após a criação do item)" 
                         type="file" 
-                        accept="image/*"
-                        capture
                         className="imagem-avatar" 
                         onChange={onChangeHandler}
                     />
